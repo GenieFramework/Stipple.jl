6689d6967880a57c4a5ee19d9b4b5fbbafdcfeb2
hhaensel <helmut.haensel@merckgroup.com>
2021-04-13 11:22:42 +0200

    add a Reactive constructor

--- src/Stipple.jl
+++ src/Stipple.jl
@@ -39,6 +39,8 @@ mutable struct Reactive{T} <: Observables.AbstractObservable{T}
   Reactive{Any}(@nospecialize(o)) = new{Any}(Observable{Any}(o), PUBLIC, false, false)
 end
 
+Reactive(v::T, args...) where T = convert(Reactive{T}, (v, args...))
+
 Base.convert(::Type{T}, x::T) where {T<:Reactive} = x  # resolves ambiguity with convert(::Type{T}, x::T) in base/essentials.jl
 Base.convert(::Type{T}, x) where {T<:Reactive} = T(x)
 
