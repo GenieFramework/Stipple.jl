f07b10e60e21f269d0cd71550eac46ffe11bfd3e
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-10-08 19:53:26 +0530

    Added API for @showif

--- src/Elements.jl
+++ src/Elements.jl
@@ -307,6 +307,24 @@ macro on(args, expr)
   :( "v-on:$(string($(esc(args))))='$(replace($(esc(expr)),"'" => raw"\'"))'" )
 end
 
+
+"""
+    `@showif(expr, [type])`
+
+v-show will always be rendered and remain in the DOM; v-show only toggles the display CSS property of the element.
+<https://vuejs.org/v2/guide/conditional.html#v-show>
+
+Difference between @showif and @iif when to use either
+
+v-if has higher toggle costs while v-show has higher initial render costs
+
+### Example
+
+```julia
+julia> h1("Hello!", @showif(:ok))
+"<h1 v-show="ok">Hello!</h1>"
+```
+"""
 macro showif(expr)
   :( "v-show='$($(esc(expr)))'" )
 end
