c72f41ee79d0f08946d384a3b8bd33618d9bbf28
hhaensel <helmut.haensel@gmx.de>
2021-11-25 14:12:06 +0100

    improve number handling of stipple_parse

--- src/Stipple.jl
+++ src/Stipple.jl
@@ -603,8 +603,12 @@ function stipple_parse(::Type{T}, value::Dict) where {Tval, T <: AbstractDict{Sy
   T(zip(Symbol.(string.(keys(value))), values(value)))
 end
 
-function stipple_parse(::Type{<:AbstractFloat}, value::T) where T <: Integer
-  convert(valtype, value)
+function stipple_parse(::Type{T1}, value::T2) where {T1 <: Number, T2 <: Number}
+  convert(T1, value)
+end
+
+function stipple_parse(::Type{T1}, value::T2) where {T1 <: Int, T2 <: Number}
+  round(T1, value)
 end
 
 function stipple_parse(::Type{T1}, value::T2) where {T1 <: AbstractArray, T2 <: AbstractArray}
