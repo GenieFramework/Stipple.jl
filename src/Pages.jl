600d75f7e6f213129b85a676d338a1e45e2c88dd
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-09-29 19:20:41 +0300

    Pages fixes

--- src/Pages.jl
+++ src/Pages.jl
@@ -27,17 +27,23 @@ const _pages = Page[]
 pages() = _pages
 
 function Page(  route::Union{Route,String};
-                view::Union{Genie.Renderers.FilePath,<:AbstractString,ParsedHTMLString},
+                view::Union{Genie.Renderers.FilePath,<:AbstractString,ParsedHTMLString,Vector{T}},
                 model::Union{M,Function,Nothing,Expr} = Stipple.init(EmptyModel),
                 layout::Union{Genie.Renderers.FilePath,<:AbstractString,ParsedHTMLString,Nothing} = nothing,
                 context::Module = @__MODULE__,
                 kwargs...
-              ) where {M<:ReactiveModel}
+              ) where {M<:ReactiveModel,T<:AbstractString}
+
+  view =  if isa(view, ParsedHTMLString) || isa(view, Vector{T})
+            string(view)
+          elseif isa(view, AbstractString)
+            isfile(view) ? filepath(view) : view
+          else
+            view
+          end
+
   isa(model, Expr) && (model = Core.eval(context, model))
   route = isa(route, String) ? Route(; method = GET, path = route) : route
-  view = isa(view, String) && isfile(view) ? filepath(view) :
-          isa(view, ParsedHTMLString) ? string(view) :
-            view
   layout = isa(layout, String) && isfile(layout) ? filepath(layout) :
             isa(layout, ParsedHTMLString) || isa(layout, String) ? string(layout) :
               layout
