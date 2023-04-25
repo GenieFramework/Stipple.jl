8d4a8eadf64c13fa9bc7ec284139f16963c6f03f
Helmut Hänsel <helmut.haensel@gmx.de>
2023-04-25 23:31:20 +0200

    new parsers for filling Arrays, e.g. Vector to Matrix

--- src/stipple/parsers.jl
+++ src/stipple/parsers.jl
@@ -13,12 +13,19 @@ function stipple_parse(::Type{T1}, value::T2) where {T1 <: Integer, T2 <: Number
   round(T1, value)
 end
 
-function stipple_parse(::Type{T1}, value::T2) where {T1 <: AbstractArray, T2 <: AbstractArray}
+# AbstractArray's of same dimension
+function stipple_parse(::Type{T1}, value::T2) where {N, T1 <: AbstractArray{<:Any, N}, T2 <: AbstractArray{<:Any, N}}
   T1(stipple_parse.(eltype(T1), value))
 end
 
-function stipple_parse(::Type{T}, value) where T <: AbstractArray
-  convert(T, eltype(T)[value])
+# atomic value to Array
+function stipple_parse(::Type{T}, value) where {N, T <: AbstractArray{<:Any, N}}
+  convert(T, Array{eltype(T), N}(reshape([value], fill(1, N)...)))
+end
+
+# Vector to Matrix, in particular Vector of Vectors to Matrix
+function stipple_parse(::Type{T1}, value::T2) where {T1 <: AbstractArray{<:Any, 2}, T2 <: AbstractArray{<:Any, 1}}
+  reduce(hcat, stipple_parse.(Vector{eltype(T1)}, value))
 end
 
 function stipple_parse(::Type{T}, value) where T <: AbstractRange
@@ -29,18 +36,21 @@ function stipple_parse(::Type{T}, v::T) where {T}
   v::T
 end
 
+# String to Symbol
 function stipple_parse(::Type{Symbol}, s::String)
   Symbol(s)
 end
-
+# untyped Dicts to typed Dict's
 function stipple_parse(::Type{<:AbstractDict{K, V}}, value::AbstractDict{String, <:Any}) where {K, V}
   Dict( zip(Vector{K}(stipple_parse(Vector{K}, collect(keys(value)))), stipple_parse(Vector{V}, collect(values(value)))) )
 end
 
-function stipple_parse(::Type{Integer}, value::String)
-  stipple_parse(Int, value)
+# String to Integer
+function stipple_parse(::Type{T}, value::String) where T <: Integer
+  Base.parse(T, value)
 end
 
-function stipple_parse(::Type{AbstractFloat}, value::String)
-  stipple_parse(Float64, value)
+#String to AbstractFloat
+function stipple_parse(::Type{T}, value::String) where T<:AbstractFloat
+  Base.parse(T, value)
 end
