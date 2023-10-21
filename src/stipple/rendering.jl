d1bb17f52b9f856b7cc91588bcc6333d75986d9b
Helmut Hänsel <helmut.haensel@gmx.de>
2023-10-21 23:00:16 +0200

    improve docstring for js_attr

--- src/stipple/rendering.jl
+++ src/stipple/rendering.jl
@@ -160,6 +160,18 @@ end
     js_attr(x)
 
 Renders a Julia expression as Javascript Expression that can be passed as an attribute value in html elements.
+### Example
+```
+using StippleUI
+
+quasar(
+  :btn__toggle,
+  fieldname = :btn_value,
+  options = js_attr([opts(label = "Off", value = false), opts(label = "On", value = true)])
+)
+
+# "<q-btn-toggle v-model=\\"btn_value\\" :options=\\"[{'value':false,'label':'Off'}, {'value':true,'label':'On'}]\\"></q-btn-toggle>"
+```
 """
 function js_attr(x)
   Symbol(replace(json(render(x)), "'" => raw"\'", '"' => '''))
