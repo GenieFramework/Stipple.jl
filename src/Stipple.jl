4a9607b71a27d76ccd19e6327d220be974a516b4
hhaensel <31985040+hhaensel@users.noreply.github.com>
2024-02-26 21:09:40 +0100

    Precompile a test app to reduce time to first webpage (#264)
    * precompile ReactiveTools App to reduce ttfx
    * optionally set port for or entirely switch off precompilation of `get()` by setting `ENV["STIPPLE_PRECOMPILE_PORT"]` (e.g. `8081`) or `ENV["STIPPLE_PRECOMPILE_GET"]` (e.g. `1`, `0`, `true`, `false`)

--- src/Stipple.jl
+++ src/Stipple.jl
@@ -15,6 +15,7 @@ existing Vue.js libraries.
 """
 module Stipple
 
+using PrecompileTools
 const PRECOMPILE = Ref(false)
 const ALWAYS_REGISTER_CHANNELS = Ref(true)
 const USE_MODEL_STORAGE = Ref(true)
@@ -1257,5 +1258,45 @@ include("Layout.jl")
 @reexport using .Typography
 @reexport using .Elements
 @reexport using .Layout
+
+# precompilation ...
+
+using Stipple.ReactiveTools
+@setup_workload begin
+  # Putting some things in `setup` can reduce the size of the
+  # precompile file and potentially make loading faster.
+  using Genie.HTTPUtils.HTTP
+  PRECOMPILE[] = true
+  @compile_workload begin
+      # all calls in this block will be precompiled, regardless of whether
+      # they belong to your package or not (on Julia 1.8 and higher)
+      ui() = [cell("hello"), row("world"), htmldiv("Hello World")]
+
+      @app PrecompileApp begin
+        @in demo_i = 1
+        @out demo_s = "Hi"
+
+        @onchange demo_i begin
+          println(demo_i)
+        end
+      end
+
+      route("/") do 
+        model = Stipple.ReactiveTools.@init PrecompileApp
+        page(model, ui) |> html
+      end
+      port = tryparse(Int, get(ENV, "STIPPLE_PRECOMPILE_PORT", ""))
+      port === nothing && (port = rand(8081:8999))
+      up(port)
+        
+      precompile_get = tryparse(Bool, get(ENV, "STIPPLE_PRECOMPILE_GET", "1"))
+      precompile_get === true && HTTP.get("http://localhost:$port")
+      # The following lines (still) produce an error although
+      # they pass at the repl. Not very important though.
+      # HTTP.get("http://localhost:$port$(Genie.Assets.asset_path(Genie.assets_config, :js, file = "channels"))")
+      # HTTP.get("http://localhost:$port$(Genie.Assets.asset_path(assets_config, :js, file = "stipplecore"))")
+      down()
+  end
+end
 
 end
